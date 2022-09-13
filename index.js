93316226cc52f38e882128312a71073b7f411225
Marcela Mejia <mejiao24@hotmail.com>
2022-09-13 17:22:55 +1000

    Initialise express

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
+// Import express
+const express = require('express');
+
 // Import the database connection
 const db = require('./config/connection');
+
+// Initialise express
+const app = express();
