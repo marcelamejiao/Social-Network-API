51a41cbebc622a1cdd2060c9c79168db06457260
Marcela Mejia <mejiao24@hotmail.com>
2022-09-13 17:28:24 +1000

    Start express

--- index.js
+++ index.js
@@ -4,5 +4,15 @@ const express = require('express');
 // Import the database connection
 const db = require('./config/connection');
 
+// Get the listening PORT
+const PORT = process.env.PORT || 3001;
+
 // Initialise express
 const app = express();
+
+// When database is connected, start express
+db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+    });
+  });
