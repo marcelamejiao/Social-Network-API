cc1f8b00604ad627c2e5c602aae3aec6a2a75c0e
Marcela Mejia <mejiao24@hotmail.com>
2022-09-15 22:35:54 +1000

    Implement models

---
+++ models/User.js
@@ -0,0 +1,49 @@
+const { Schema, model } = require('mongoose');
+
+// Schema to create User model
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trimmed: true,
+        },
+        email: { 
+            type: String,
+            required: true,
+            unique: true,
+            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, "Email address is invalid."]
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'thought',
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'user',
+            },
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+    }
+);
+
+// Create a virtual property `friendCount`
+userSchema
+  .virtual('friendCount')
+  // Getter
+  .get(function () {
+    return this.friends.length;
+  });
+
+const User = model('user', userSchema);
+
+module.exports = User;
