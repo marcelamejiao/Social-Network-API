21e5fd4ad04f6aad2e485fef9928fce3cc62ad5f
Marcela Mejia <mejiao24@hotmail.com>
2022-09-16 19:42:33 +1000

    Fix typo in thought routes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -7,13 +7,13 @@ const {
   deleteThought,
   addReaction,
   removeReaction,
-} = require('../../controllers/userController');
+} = require('../../controllers/thoughtController');
 
 // /api/thoughts - Get all thoughts and create a new thought
 router.route('/').get(getThoughts).post(createThought);
 
 // /api/thoughts/:thoughtId - Get one thought, update a thought and delete a thought
-router.route('/:userId').get(getSingleThought).put(updateThought).delete(deleteThought);
+router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
 
 // ///api/thoughts/:thoughtId/reactions - Add a new reaction and delete reaction
 router.route('/:/api/thoughts/:thoughtId/reactions').post(addReaction).delete(removeReaction);
