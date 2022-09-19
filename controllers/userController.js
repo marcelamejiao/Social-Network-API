452922516c6fa3a100326da636f465a847d40697
Marcela Mejia <mejiao24@hotmail.com>
2022-09-19 18:06:16 +1000

    Fix population

--- controllers/userController.js
+++ controllers/userController.js
@@ -17,8 +17,9 @@ module.exports = {
     // Get a single user
     getSingleUser(req, res) {
       User.findOne({ _id: req.params.userId })
-        .populate('thoughts', 'friends')
         .select('-__v')
+        .populate({ path: 'thoughts', select: '-__v' })
+        .populate({ path: 'friends', select: '-__v' })
         .then(async (user) =>
           !user
             ? res.status(404).json({ message: 'No user with that ID' })
